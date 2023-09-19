3ab6d09ec8dc52ac78f37ef4118c04c0d1fadf96
Jkomba <johnkombah88@gmail.com>
2023-09-19 10:25:50 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42);
+}
+function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    const blocks = Math.abs(startBlock - endBlock);
+    return blocks * 264;
+}
+function calculatesFarePrice(startBlock, endBlock) {
+    const distance = distanceTravelledInFeet(startBlock, endBlock);
+
+    if (distance <= 400) {
+        return 0; // The first 400 feet are free
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02; // 2 cents per foot for distance between 400 and 2000 feet
+    } else if (distance > 2000 && distance < 2500) {
+        return 25; // Flat fare for distance over 2000 feet and under 2500 feet
+    } else {
+        return 'cannot travel that far'; // Scuber does not allow rides over 2500 feet
+    }
+}
